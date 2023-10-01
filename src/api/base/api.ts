4fbec7cf7ebe5f3b3d3a6c2f4b7357bdec707b05
NickSettler <moiseevnikita14@gmail.com>
2023-10-01 16:59:40 +0200

    feat(api): add basic api class for requests

--- src/api/base/api.ts
+++ src/api/base/api.ts
@@ -1,17 +1,84 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
+/**
+ * API class (Singleton)
+ * @class Api
+ */
 export default class Api {
-  private static _instance: AxiosInstance;
+  /**
+   * API instance
+   * @private
+   */
+  private static _instance: Api;
 
-  private static createInstance(): AxiosInstance {
-    return axios.create({
-      url: process.env.REACT_APP_API_URL,
+  /**
+   * Axios instance
+   * @private
+   */
+  private _axiosInstance: AxiosInstance;
+
+  private constructor() {
+    this._axiosInstance = axios.create({
+      baseURL: process.env.REACT_APP_API_URL,
+    });
+
+    this._axiosInstance.interceptors.request.use((config) => {
+      console.log(config);
+      return config;
     });
   }
 
+  /**
+   * Get API instance
+   */
   public static get instance(): Api {
-    if (!this._instance) this._instance = this.createInstance();
+    if (!this._instance) this._instance = new Api();
 
-    return this;
+    return this._instance;
+  }
+
+  /**
+   * Send GET request to API
+   * @param url - API endpoint
+   */
+  public async get<Response>(url: string): Promise<Response> {
+    return await this._axiosInstance
+      .get<Response>(url)
+      .then((response) => response.data);
+  }
+
+  /**
+   * Send POST request to API
+   * @param url - API endpoint
+   * @param data - Data to send
+   */
+  public async post<Data, Response>(
+    url: string,
+    data: Data,
+  ): Promise<Response> {
+    return await this._axiosInstance
+      .post<Response, AxiosResponse<Response>, Data>(url, data)
+      .then((response) => response.data);
+  }
+
+  /**
+   * Send PUT request to API
+   * @param url - API endpoint
+   * @param data - Data to send
+   */
+  public async put<Data, Response>(url: string, data: Data): Promise<Response> {
+    return await this._axiosInstance
+      .put<Response, AxiosResponse<Response>, Data>(url, data)
+      .then((response) => response.data);
+  }
+
+  /**
+   * Send DELETE request to API
+   * @param url - API endpoint
+   */
+  public async delete<Response>(url: string): Promise<Response> {
+    return await this._axiosInstance
+      .delete<Response>(url)
+      .then((response) => response.data);
   }
 }
