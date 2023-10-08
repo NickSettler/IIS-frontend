8a3098772ff90a54148ccddfe5f3b186b13ed265
Nikita Pasynkov <pasynkov.n.d@gmail.com>
2023-10-08 23:40:16 +0200

    feat: add basic routes for demonstration

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const HomePage = (): ReactElement => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const LogInPage = (): ReactElement => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LogInPage;
