94800ae7ec31cb1ebfbee063c996a769cb5686fe
NickSettler <moiseevnikita14@gmail.com>
2023-10-11 03:45:47 +0200

    feat: add react query and mui theme providers

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { client } from './utils/react-query/client';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './utils/theme/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -13,7 +15,9 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+      </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
