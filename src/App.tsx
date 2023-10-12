a05648c73d9af8e4ff8c2bd59cb7b14001f5d611
Nikita Pasynkov <pasynkov.n.d@gmail.com>
2023-10-12 19:46:28 +0200

    feat: add sidebar menu and routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { ReactElement } from 'react';
 import './App.css';
-import { useRoutes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
 import LogInPage from './pages/login';
 import HomePage from './pages/home';
 
@@ -15,8 +20,54 @@ const routes = [
   },
 ];
 
+const SideBarMenu = (): ReactElement => {
+  return (
+    <Box
+      component='nav'
+      sx={{
+        width: '200px',
+        height: '100vh',
+        backgroundColor: '#f5f5f5',
+      }}
+    >
+      <List>
+        <Link to='/'>
+          <ListItemButton>
+            <ListItemText primary='Home' />
+          </ListItemButton>
+        </Link>
+        <Link to='/login'>
+          <ListItemButton>
+            <ListItemText primary='Login' />
+          </ListItemButton>
+        </Link>
+      </List>
+    </Box>
+  );
+};
+
 const App = (): ReactElement | null => {
-  return useRoutes(routes);
+  return (
+    <div
+      className='page'
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <Routes>
+        {routes.map(({ path }) => (
+          <Route key={path} path={path} element={<SideBarMenu />} />
+        ))}
+      </Routes>
+
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
