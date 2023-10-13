799836fe597cf22f82dfd6db3fc4d987a4077fdc
Nikita Pasynkov <pasynkov.n.d@gmail.com>
2023-10-13 21:45:50 +0200

    fix: add first name and last name for sign-ip endpoint

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -7,6 +7,13 @@ export type TAuthLoginMutationVariables = {
   password: string;
 };
 
+export type TAuthRegisterMutationVariables = {
+  [E_USER_ENTITY_KEYS.USERNAME]: string;
+  password: string;
+  [E_USER_ENTITY_KEYS.FIRST_NAME]: string;
+  [E_USER_ENTITY_KEYS.LAST_NAME]: string;
+};
+
 export type TAuthLoginResponse = {
   accessToken: string;
   refreshToken: string;
@@ -28,11 +35,18 @@ export default class AuthService extends BaseService {
 
   public static async signUp(
     username: TUser[E_USER_ENTITY_KEYS.USERNAME],
+    firstName: string,
+    lastName: string,
     password: string,
   ): Promise<TAuthLoginResponse> {
     return await Api.instance.post<
-      TAuthLoginMutationVariables,
+      TAuthRegisterMutationVariables,
       TAuthLoginResponse
-    >(`${this.endpoint}/sign-up`, { username, password });
+    >(`${this.endpoint}/sign-up`, {
+      [E_USER_ENTITY_KEYS.USERNAME]: username,
+      [E_USER_ENTITY_KEYS.FIRST_NAME]: firstName,
+      [E_USER_ENTITY_KEYS.LAST_NAME]: lastName,
+      password,
+    });
   }
 }
