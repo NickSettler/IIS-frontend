0998b323b49c0f1882b53a762ee6074ac775e5bb
Nikita Pasynkov <pasynkov.n.d@gmail.com>
2023-10-14 17:04:07 +0200

    feat: add sign in form

---
+++ src/components/authorization/sign-in/index.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const SignIn = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <Box
+          component='form'
+          onSubmit={handleSubmit}
+          noValidate={true}
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin='normal'
+            required={true}
+            fullWidth={true}
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus={true}
+          />
+          <TextField
+            margin='normal'
+            required={true}
+            fullWidth={true}
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
+          />
+          <Button
+            type='submit'
+            fullWidth={true}
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Link href='/' variant='body2'>
+            Don&apos;t have an account? Sign Up
+          </Link>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default SignIn;
