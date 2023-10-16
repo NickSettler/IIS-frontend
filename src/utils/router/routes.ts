1af463b0967f25bc1d614c24a425b840ed9072ef
NickSettler <moiseevnikita14@gmail.com>
2023-10-17 00:52:18 +0200

    feat(users): add users page

--- src/utils/router/routes.ts
+++ src/utils/router/routes.ts
@@ -2,6 +2,7 @@ import { RouteObject } from 'react-router-dom';
 import HomePage from '../../pages/home';
 import LogInPage from '../../pages/login';
 import SignUpPage from '../../pages/register';
+import { UsersDataTable } from '../../components/users/data-table';
 
 export type TAppRoute = RouteObject & {
   path: string;
@@ -23,5 +24,10 @@ export const appRoutes: Array<TAppRoute> = [
     path: '/register',
     label: 'Register',
     Component: SignUpPage,
+  },
+  {
+    path: '/users',
+    label: 'Users',
+    Component: UsersDataTable,
   },
 ];
