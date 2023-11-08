7978336be7499aa95d6b0db72fd61ee58325e9f7
NickSettler <moiseevnikita14@gmail.com>
2023-11-08 20:58:14 +0100

    feat(teacher-req): add teacher requirement types and service

---
+++ src/api/teacher-requirements/types.ts
@@ -0,0 +1,22 @@
+import { TApiUser } from '../user/types';
+
+export enum E_TEACHER_REQUIREMENT_ENTITY_KEYS {
+  ID = 'id',
+  MODE = 'mode',
+  TEACHER = 'teacher',
+  START_TIME = 'start_time',
+  END_TIME = 'end_time',
+}
+
+export enum E_TEACHER_REQUIREMENT_MODE {
+  EXCLUDE = 'EXCLUDE',
+  INCLUDE = 'INCLUDE',
+}
+
+export type TTeacherRequirement = {
+  [E_TEACHER_REQUIREMENT_ENTITY_KEYS.ID]: string;
+  [E_TEACHER_REQUIREMENT_ENTITY_KEYS.MODE]: E_TEACHER_REQUIREMENT_MODE;
+  [E_TEACHER_REQUIREMENT_ENTITY_KEYS.TEACHER]: TApiUser;
+  [E_TEACHER_REQUIREMENT_ENTITY_KEYS.START_TIME]: Date;
+  [E_TEACHER_REQUIREMENT_ENTITY_KEYS.END_TIME]: Date;
+};
