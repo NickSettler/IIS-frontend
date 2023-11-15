b3de07cadbc98323fa09211d02e621c478266ec0
NickSettler <moiseevnikita14@gmail.com>
2023-11-15 02:22:26 +0100

    feat(schedule): add schedule types

---
+++ src/api/schedule/types.ts
@@ -0,0 +1,23 @@
+import { TApiUser } from '../user/types';
+import { TCourseActivity } from '../course-activities/types';
+import { TClass } from '../class/types';
+
+export enum E_SCHEDULE_ITEM_ENTITY_KEYS {
+  ID = 'id',
+  COURSE_ACTIVITY = 'course_activity',
+  TEACHER = 'teacher',
+  CLASS = 'class',
+  START_TIME = 'start_time',
+  END_TIME = 'end_time',
+  RECURRENCE_RULE = 'recurrence_rule',
+  EXCLUSION_DATES = 'exclusion_dates',
+}
+
+export type TScheduleItem = {
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.ID]: string;
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.COURSE_ACTIVITY]: TCourseActivity;
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.TEACHER]: TApiUser;
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.CLASS]: TClass;
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.START_TIME]: Date;
+  [E_SCHEDULE_ITEM_ENTITY_KEYS.END_TIME]: Date;
+};
